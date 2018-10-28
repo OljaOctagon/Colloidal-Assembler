1696439a4c1bae0b40e3bc0d7b077a18673ea908
Cat Nate <catnat@protonmail.com>
2018-10-28 18:32:02 +0100

    bug fix in calc_domain_size.py

--- utils/domain_size.py
+++ utils/domain_size.py
@@ -1,7 +1,7 @@
 import networkx as nx
 import pandas as pd
 import numpy as np 
-
+import matplotlib.pyplot as plt
 # This script calculates the domain size of parallel and non-parallel bonds of the largest cluster in the rhombi system.
 # We interpret the largest cluster as two networks. One with all bondings of 1 (parallel) and one with all bondings of -1 (non-parallel).
 # The number of connected components gives the domain sizes. We then calculate the distribution, the mean, and the variance. 
@@ -14,7 +14,8 @@ def calculate_network_domains(arr, p_index):
     G.add_edges_from(arr_p[:,:2])
     domains = list(nx.connected_components(G))
     domain_sizes = [ len(domain) for domain in domains]
-    print(domain_sizes)
+    plt.hist(domain_sizes)
+    plt.savefig('domain_sizes_{}.png'.format('p_index') dpi=300)
     mean=np.mean(domain_sizes)
     std = np.std(domain_sizes)
     cluster_size=arr[0,3]
