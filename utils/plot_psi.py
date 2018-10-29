713aa7eb9c888af7ca92e26ad4e83b8e93961f65
Cat Nate <catnat@protonmail.com>
2018-10-29 10:08:01 +0100

    add psi plot functionality

--- utils/plot_psi.py
+++ utils/plot_psi.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+import json
 sns.set(style='white')
 
 # ncluster > 400
@@ -16,13 +16,26 @@ x = np.array([[0.028080306781609193, 0.04677896872550968],
 fig,ax = plt.subplots()
 ax.set_aspect(0.99)
 plt.ylim([-1,1])
-ax.spines['right'].set_visible(False)
-ax.spines['top'].set_visible(False)
-ax.spines['bottom'].set_visible(False)
-ax.get_xaxis().set_visible(False)
-ax.yaxis.set_ticks_position('left')
+#ax.spines['right'].set_visible(False)
+#ax.spines['top'].set_visible(False)
+#ax.spines['bottom'].set_visible(False)
+#ax.get_xaxis().set_visible(False)
+#ax.yaxis.set_ticks_position('left')
+
+# plot single compairsion lines at center position
 for i in range(len(x)):
     ax.errorbar(0, x[i,0], yerr=x[i,1], color=color[i], elinewidth=3, capsize=2, label = names[i], marker=marker[i], ms=8)
+
+def parse_json(filen):
+    with open(filen) as fhandle:
+        data = json.load(fhandle)
+    print(data)
+    return data
+# plot assym manta
+ad_manta = parse_json("psi_mean_assym_manta.json")
+
+# plot assym mouse
+ad_mouse = parse_json("psi_mean_assym_mouse.json")
 
 plt.legend(loc='best')
 plt.savefig("whitlam_comparision.png", bbox_inches='tight', dpi=300)
