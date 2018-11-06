328fd8b1425a6edde674a4d147ca9b7b1e4fea53
Cat Nate <catnat@protonmail.com>
2018-11-06 16:25:41 +0100

    latex table template generation

--- utils/create_domain_size_table.py
+++ utils/create_domain_size_table.py
@@ -59,12 +59,12 @@ def generate_table(n_lines,n_col, table_dict, names):
 
     column_format='| '
     for i in range(n_col): column_format + 'l |'
-    start_tabular='\begin{{tabular}}{{}}'.format(column_format)
+    start_tabular='\\begin{{tabular}}\{{}\}'.format(column_format)
     end_tabular='\end{tabular}'
     hline='\hline'
     breakline='\\'
-    line_legend= 'particle type'
-    for name in names: line_legend = line_legend + '{} &'.format(name)
+    line_legend= 'particle type & '
+    for name in names: line_legend = line_legend + ' {} &'.format(name)
     col_legend=''
     for key in table_dict.keys(): col_legend = col_legend + '& {}'.format(key)
 
