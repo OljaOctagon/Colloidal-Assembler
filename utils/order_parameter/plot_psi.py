9f95b007c61ec09f3bf7144f37fcecc422fd0ea3
Cat Nate <catnat@protonmail.com>
2018-11-12 14:47:35 +0100

    updated phase diagram plot and psi plot

--- utils/order_parameter/plot_psi.py
+++ utils/order_parameter/plot_psi.py
@@ -2,12 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import json
+from matplotlib import rc
+rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
+## for Palatino and other serif fonts use:
+#rc('font',**{'family':'serif','serif':['Palatino']})
+rc('text', usetex=True)
+
+
 sns.set(style='white')
 
 # ncluster > 400
 names = ['dma-c', 'dmo-c', 'whitlam et al', 'comparison']
 marker=['o', 'X', 's', 'd']
-color=['r', 'b', 'g', 'y']
+color=['r', 'b', 'k', 'g']
 x = np.array([[0.028080306781609193, 0.04677896872550968],
               [0.5183065888324874, 0.021978519622216603],
               [-0.082800232105,	 0.05],
@@ -20,7 +27,7 @@ plt.ylim([-1.1,1.1])
 plt.yticks(np.arange(-1,1.1,0.5))
 plt.xticks(np.arange(0.5,0.9,0.1))
 plt.xlabel("$\Delta$", size=18)
-plt.ylabel("$\phi$", size=18)
+plt.ylabel("$\psi$", size=18)
 #ax.spines['right'].set_visible(False)
 #ax.spines['top'].set_visible(False)
 #ax.spines['bottom'].set_visible(False)
@@ -50,22 +57,40 @@ def plot_error_curve(data, name, marker, color):
                  capsize=2,
                  marker=marker,
                  ms=8,
-                 alpha=0.4,
+                 alpha=0.7,
+                 zorder=-1,
+                 label=name)
+
+
+def plot_synthetic_error_curve(x,y, name, marker, color):
+    plt.errorbar(x, y, yerr=0,
+                 color=color,
+                 elinewidth=3,
+                 capsize=2,
+                 marker=marker,
+                 ms=8,
+                 alpha=0.7,
                  zorder=-1,
                  label=name)
 
 # plot assym manta
 ad_manta = parse_json("psi_mean_assym_manta.json")
-plot_error_curve(ad_manta, 'dma-as', 'o', 'r')
+plot_error_curve(ad_manta, 'dma-as', 'o', '#ff6600')
 
 # plot assym mouse
 ad_mouse = parse_json("psi_mean_assym_mouse.json")
-plot_error_curve(ad_mouse, 'dmo-as', 'X', 'b')
+plot_error_curve(ad_mouse, 'dmo-as', 'X', '#6600ff')
 
+# plot checkers symm
+plot_synthetic_error_curve(np.arange(0.5,0.8,0.1), np.ones(4), 'checkers-s', 'D', 'y')
+
+# plot checkers asymm
+plot_synthetic_error_curve(np.arange(0.5,0.8,0.1), np.ones(4), 'checkers-as', 'P', '#33cc33')
+
+# plot dma-s
+plot_synthetic_error_curve(np.arange(0.6,0.8,0.1), -1*np.ones(3), 'dma-s', 'o', '#cc0066')
 
 plt.legend(loc='best')
-plt.savefig("psi.png", bbox_inches='tight', dpi=300)
+plt.savefig("psi.png", bbox_inches='tight', dpi=500)
 plt.show()
-
-n_points = np.array([264,591,-1, 84])
 
--- utils/phase_diagrams/4_patch_phases/phase_diagram_plot.py
+++ utils/phase_diagrams/4_patch_phases/phase_diagram_plot.py
@@ -6,6 +6,7 @@ import json
 import itertools
 import matplotlib.patches as mpatches
 import matplotlib.transforms as transforms
+
 def size_hexagon(Lx, patch_pos):
     ''' The bigger pores are hexagon and the smaller triangles of 
     l=(fabs(patch_pos - (1-patch_pos)).'''
@@ -104,15 +105,14 @@ for filen in files:
     zvals = np.concatenate((data_7kbt, data_6kbt,
                             data_5kbt, data_4kbt), axis=0)
 
-    print(zvals)
-
     fig, ax = plt.subplots()
-    #zvals = np.random.randint(-1,1, size=(l_e,l_p))
-
     img = plt.imshow(zvals,interpolation='nearest',
                      cmap = cmap, norm = norm)
 
-    pos_start = -0.5
+
+    ax = plt.gca()
+
+    pos_start = 0
     x_og = np.arange(pos_start,pos_start+l_p,1)
     y_og = np.arange(pos_start,pos_start+l_e,1)
     ax.set_xticks(x_og)
@@ -121,11 +121,15 @@ for filen in files:
     plt.xticks(x_og, patch_values, rotation='horizontal')
     plt.yticks(y_og, energy_values, rotation='horizontal')
 
+    # Minor ticks
+    ax.set_xticks(x_og-0.5, minor=True);
+    ax.set_yticks(y_og-0.5, minor=True);
+
     plt.grid(b=True,
-             which='major', 
-             axis='both', 
+             which='minor', 
              linestyle='-', 
-             color='k', linewidth=2)
+             color='k',
+             linewidth=2)
 
     draw_pores(patch_values, energy_values, pore_dict[filen], poly_function_dict[filen])
     plt.xlabel("$\\Delta$", size=20)
@@ -138,37 +142,3 @@ for filen in files:
     plt.savefig("phase_diagram_{}.png".format(name_dict[filen]), dpi=300)
 
 plt.show()
-
-'''
-# setup the plot
-fig, ax = plt.subplots(1,1, figsize=(6,6))
-
-# define the data
-x = np.random.rand(20)
-y = np.random.rand(20)
-tag = np.random.randint(0,20,20)
-tag[10:12] = 0 # make sure there are some 0 values to showup as grey
-
-# define the colormap
-cmap = plt.cm.jet
-# extract all colors from the .jet map
-cmaplist = [cmap(i) for i in range(cmap.N)]
-# force the first color entry to be grey
-cmaplist[0] = (.5,.5,.5,1.0)
-# create the new map
-cmap = cmap.from_list('Custom cmap', cmaplist, cmap.N)
-
-# define the bins and normalize
-bounds = np.linspace(0,20,21)
-norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
-
-# make the scatter
-scat = ax.scatter(x,y,c=tag,s=np.random.randint(100,500,20),cmap=cmap, norm=norm)
-
-# create a second axes for the colorbar
-ax2 = fig.add_axes([0.95, 0.1, 0.03, 0.8])
-cb = mpl.colorbar.ColorbarBase(ax2, cmap=cmap, norm=norm, spacing='proportional', ticks=bounds, boundaries=bounds, format='%1i')
-
-ax.set_title('Well defined discrete colors')
-ax2.set_ylabel('Very custom cbar [-]', size=12)
-'''
