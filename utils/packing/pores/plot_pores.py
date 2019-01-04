4d5275a2b52665c9bb6a0b04f69877baf4f1c678
Cat Nate <catnat@protonmail.com>
2019-01-04 13:42:16 +0100

    renamed hex_pore_plot.py to plot_pores.py

---
+++ utils/packing/pores/plot_pores.py
@@ -0,0 +1,68 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+
+
+def area_hexagon(Lx, patch_pos):
+    ''' The bigger pores are hexagon and the smaller triangles of 
+    l=(fabs(patch_pos - (1-patch_pos)).'''
+    pore_p = np.fabs(2*patch_pos - 1)
+    pore_size = pore_p*Lx
+    pore_area = (3*np.sqrt(3)/2)*np.power(pore_size,2)
+    return pore_area
+
+
+def area_triangle(Lx, patch_pos):
+    pore_p = np.fabs(2*patch_pos - 1)
+    pore_size = pore_p*Lx
+    pore_area = (np.sqrt(3)/(6)*np.power(pore_size,2))
+    return pore_area
+
+
+
+def area_rhombi(Lx,patch_pos):
+    ''' the unit cell has one rhombic pore. 
+    Its area is a function of the patch size. '''
+    pore_p = np.fabs(2*patch_pos - 1)
+    pore_size = pore_p*Lx
+    pore_area = np.power(pore_size,2)*np.sin(np.pi/3.)
+    return pore_area
+
+
+import matplotlib.pyplot as plt 
+from matplotlib import rc 
+import seaborn as sns 
+
+rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
+rc('text', usetex=True)
+#sns.set(style='whitegrid')
+
+#-----------
+
+Lx = 1
+patch_pos = np.arange(0,1.05,0.1)
+area_h = area_hexagon(Lx, patch_pos)
+area_r = area_rhombi(Lx,patch_pos)
+area_t = area_triangle(Lx, patch_pos)
+
+fig, ax = plt.subplots()
+ax.plot(patch_pos,area_h, lw=2, marker='h', c='#336699', 
+    label='$\displaystyle \\textrm{hexagonal pore}$',
+        alpha=1.0)
+
+ax.plot(patch_pos,area_t, lw=2, marker='^', c='#000080', 
+    label='$\displaystyle \\textrm{triangular pore}$',
+        alpha=1.0)
+
+ax.plot(patch_pos,area_r, lw=2, marker='D', c='r', 
+    label='$\displaystyle \\textrm{rhombic pore}$',
+        alpha=1.0)
+
+ax.set_xlim([0,1])
+#ax.set_ylim([0.0,1.03])
+ax.legend(loc='upper center', fancybox=True, prop={'size': 21}, framealpha=1.0)
+ax.tick_params(axis='both', which='major', labelsize=22)
+ax.set_xlabel("$ \displaystyle \Delta$", size=30)
+ax.set_ylabel("$ \displaystyle \\textrm{pore area} [1/l^2]$", size=30)
+plt.tight_layout()
+plt.savefig("pore_area_function.png", dpi=400)
+plt.show()
