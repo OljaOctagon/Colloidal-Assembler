e1541239e175811f4e56ff96f7780854f90feac0
Cat Nate <catnat@protonmail.com>
2019-08-30 13:47:20 +0200

    plot_nematic.py: fraction and nematic order in one plot

--- utils/phase_diagrams/2_patch_phases/plot_nematic.py
+++ utils/phase_diagrams/2_patch_phases/plot_nematic.py
@@ -8,7 +8,7 @@ df = pd.read_csv("nematic_op.csv")
 df_as =  pd.read_csv("nematic_op_as.csv")
 
 
-def plot_observable(df, df_as, ylabel, parameter, name):
+def plot_observable(ax, df, df_as, ylabel, parameter, name):
 
     arr_local = df.groupby(
             ['delta'])[parameter].agg(
@@ -21,7 +21,7 @@ def plot_observable(df, df_as, ylabel, parameter, name):
 
     arr_local[3] = brr_local[3]
 
-    plt.errorbar(arr_local[:,0],
+    ax.errorbar(arr_local[:,0],
                 arr_local[:,1],
                 arr_local[:,2],
                 label='pl-s', marker='o',
@@ -30,7 +30,7 @@ def plot_observable(df, df_as, ylabel, parameter, name):
                 capthick=5,
                 lw=1)
 
-    plt.errorbar(brr_local[:,0],
+    ax.errorbar(brr_local[:,0],
                 brr_local[:,1],
                 brr_local[:,2],
                 label='pl-as', marker='o',
@@ -39,7 +39,7 @@ def plot_observable(df, df_as, ylabel, parameter, name):
                 capthick=5,
                 lw=1)
 
-    plt.errorbar(brr_local[3,0],
+    ax.errorbar(brr_local[3,0],
                 brr_local[3,1],
                 brr_local[3,2],
                 label='pl-center', marker='o',
@@ -47,29 +47,27 @@ def plot_observable(df, df_as, ylabel, parameter, name):
                 capsize=5,
                 capthick=5,
                 lw=1)
+    ax.set_title(name, size=18)
+    ax.tick_params(axis='both', labelsize=16)
+    ax.set_xlabel("$\Delta$", size=18)
+    ax.set_ylabel(ylabel, size=18)
+    ax.legend(loc='best')
+    ax.set_ylim([0,1])
 
-    plt.tick_params(axis='both', labelsize=20)
-    plt.xlabel("$\Delta$", size=22)
-    plt.ylabel(ylabel, size=22)
-    plt.legend(loc='best')
-    plt.ylim([0,1])
-    plt.tight_layout()
-    plt.savefig("{}.png".format(name), dpi=300)
-
-
-fig,ax = plt.subplots()
+fig,(ax2,ax1)  = plt.subplots(nrows=1,ncols=2, figsize=(13,7))
 cmap=plt.cm.Dark2
 c = cycler('color', cmap(np.linspace(0,1,4)) )
-ax.set_prop_cycle(c)
+ax1.set_prop_cycle(c)
 ylabel = "$S_{largest}$"
 
-plot_observable(df, df_as, ylabel, 'local_nematic_op', 'local_nematic')
+plot_observable(ax1, df, df_as, ylabel, 'local_nematic_op', 'Nematic order of largest cluster')
 
-
-fig,ax = plt.subplots()
 cmap=plt.cm.jet
 c = cycler('color', cmap(np.linspace(0,1,4)) )
-ax.set_prop_cycle(c)
+ax2.set_prop_cycle(c)
 ylabel = "$f_{largest}$"
-plot_observable(df, df_as, ylabel, 'fraction_largest_nematic', 'fraction_largest_nematic')
+plot_observable(ax2, df, df_as, ylabel, 'fraction_largest_nematic', 'Fraction of larget cluster')
+
+plt.tight_layout()
+plt.savefig("{}.pdf".format("pl_nematic"), dpi=300)
 
