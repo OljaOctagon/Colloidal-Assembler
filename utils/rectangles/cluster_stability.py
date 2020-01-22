b851ef67982c1bf250d3e1dad6fb5578cd796d8f
catnat <catnat@protonmail.com>
2020-01-22 15:47:28 +0100

    cluster stability/distribution code working

--- utils/rectangles/cluster_stability.py
+++ utils/rectangles/cluster_stability.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import itertools
 def read_bonds(filen):
     first_line_pair = (0,0,0,0)
     cut=False
@@ -23,10 +23,10 @@ def read_bonds(filen):
     #network_list = [ np.array(item) for item in network_list]
     return network_list
 
-if __name__ == '__main__':
-    pn_file = "patch_network.dat"
-    network_list = read_bonds("patch_network.dat")
+def runs_of_ones_list(seq):
+    return [sum(g) for b, g in itertools.groupby(seq) if b==1]
 
+def calculate_cluster_stability(network_list):
     # add padding so we can cast network_list to array
     lengths = [ len(item) for item in network_list]
     max_length = max(lengths)
@@ -36,7 +36,68 @@ if __name__ == '__main__':
         item_list.extend(padding)
 
     network_arr = np.array(network_list, dtype = 'i,i,i,i')
-    print(network_arr.shape)
-    uniques,indices,inverse, counts  = np.unique(network_arr,return_index=True, return_inverse=True, return_counts=True)
-    print(inverse.shape)
-    #print(indices[0])
+    dimx,dimy = network_arr.shape
+    uniques,inverse = np.unique(network_arr,return_inverse=True)
+    inverse = np.reshape(inverse, (dimx,dimy))
+    print(inverse)
+    lengths = []
+    for i in range(len(uniques)):
+        sequence = np.where(inverse == i)[0]
+        diff_seq = np.diff(sequence)
+        length_runs = runs_of_ones_list(diff_seq)
+        if length_runs:
+            lengths.append(length_runs)
+
+    return uniques, lengths
+
+def calc_stats_per_type(m,n, uniques, lengths):
+
+    flatten = lambda l: [item for sublist in l for item in sublist]
+    f_type = lambda i,m,n: ((uniques[i][2],uniques[i][3]) == (m,n)) or ((uniques[i][2],uniques[i][3]) == (n,m))
+
+    #print(lengths)
+    c_type_lengths = [ lengths[i] for i in range(len(lengths)) if f_type(i,m,n) ]
+    c_type_lengths = np.array(flatten(c_type_lengths))
+
+    c_mean = 0
+    c_std = 0
+
+    if c_type_lengths.size:
+        c_mean = np.mean(c_type_lengths)
+        c_std = np.std(c_type_lengths)
+
+    return c_mean, c_std
+
+
+if __name__ == '__main__':
+    pn_file = "patch_network.dat"
+    network_list = read_bonds("patch_network.dat")
+
+    # cluster stability
+    uniques, lengths = calculate_cluster_stability(network_list)
+    p_mean, p_std = calc_stats_per_type(1,1, uniques, lengths)
+    s_mean, s_std = calc_stats_per_type(0,0, uniques, lengths)
+    l_mean, l_std = calc_stats_per_type(0,1, uniques, lengths)
+
+    print("p-type", p_mean, p_std)
+    print("s-type", s_mean, s_std)
+    print("l-type", l_mean, l_std)
+
+    # cluster distribution of size 2 clusters
+    flatten = lambda l: [item for sublist in l for item in sublist]
+
+    network_list = read_bonds("patch_network.dat")
+    flat_network_list = flatten(network_list)
+
+    N_total = len(flat_network_list)
+    arr_flat = np.array(flat_network_list)
+    
+    N_p = len( arr_flat[np.where((arr_flat[:,2] == 1) & (arr_flat[:,3] == 1))[0]] )
+    print("N_p", N_p/N_total)
+
+    N_s = len( arr_flat[np.where((arr_flat[:,2] == 0) & (arr_flat[:,3] == 0))[0]] )
+    print("N_s", N_s/N_total)
+
+    N_l1 = len( arr_flat[np.where((arr_flat[:,2] == 0) & (arr_flat[:,3] == 1))[0]] )
+    N_l2 = len( arr_flat[np.where((arr_flat[:,2] == 1) & (arr_flat[:,3] == 0))[0]] )
+    print("N_l", (N_l1+N_l2)/N_total)
--- utils/visual_inspection/draw_rectangles.py
+++ utils/visual_inspection/draw_rectangles.py
@@ -52,101 +52,102 @@ def get_hexcolor(i, cmap):
 	return mpl.colors.rgb2hex(rgb)
 
 def get_domain_colors(N_particles, bond_arr, length_color_dict):
-	G = nx.Graph()
-	G.add_edges_from(bond_arr[:,:2])
-	domains = list(nx.connected_components(G))
-	domain_colors = [length_color_dict[0]]*N
-	for cluster in domains:
-		length = len(cluster)
-		if length >=5:
-			length=5
-		for particle in cluster:
-			domain_colors[particle] = length_color_dict[length]
+    domain_colors = [length_color_dict[0]]*N
 
-	return domain_colors
+    if bond_arr.size:
+        G = nx.Graph()
+        G.add_edges_from(bond_arr[:,:2])
+        domains = list(nx.connected_components(G))
+
+        for cluster in domains:
+            length = len(cluster)
+            if length >=5:
+                length=5
+            for particle in cluster:
+                domain_colors[particle] = length_color_dict[length]
+
+    return domain_colors
 
 if __name__ == '__main__':
-	# get all check point values and sort them
-	checkpoints= glob.glob("Box*.bin")
-	check_point_values = np.sort(
-	[ int(point.split("_")[-1].split(".")[0]) for point in checkpoints ])
+    # get all check point values and sort them
+    checkpoints= glob.glob("Box*.bin")
+    check_point_values = np.sort(
+    [ int(point.split("_")[-1].split(".")[0]) for point in checkpoints ])
+    # import the bonds file:
+    # file format:
+    # ------------------------
+    # #new time
+    # particle_id1 particle_id2  patch_id1 patch_id2]
+    #  ....
+    # --------------------------
 
-	# import the bonds file:
-	# file format:
-	# ------------------------
-	# #new time
-	# particle_id1 particle_id2  patch_id1 patch_id2]
-	#  ....
-	# --------------------------
-
-	pn_file = "patch_network.dat"
+    pn_file = "patch_network.dat"
 
 	# colormap for cluster size
-	cmap = plt.cm.get_cmap('cividis', 6)
-	hex_color = [ get_hexcolor(i, cmap) for i in range(6)]
-	hex_color[4] = '#8A2BE2'
-	length_color_dict = dict(zip(np.arange(6),hex_color))
+    cmap = plt.cm.get_cmap('cividis', 6)
+    hex_color = [ get_hexcolor(i, cmap) for i in range(6)]
+    hex_color[4] = '#8A2BE2'
+    length_color_dict = dict(zip(np.arange(6),hex_color))
 
-	# network_arr format: network_arr.shape = ( frame_i, bond_rows_frame_i )
-	network_arr = read_bonds("patch_network.dat")
-	# patch position calculation
-	Lx=1.0
-	Ly=2.0
-	a=0.25
-	b=0.5
-	radius=0.2
-	particle_patches = get_patches(Lx,Ly,a,b)
+    # network_arr format: network_arr.shape = ( frame_i, bond_rows_frame_i )
+    network_arr = read_bonds("patch_network.dat")
+    # patch position calculation
+    Lx=1.0
+    Ly=2.0
+    a=0.25
+    b=0.5
+    radius=0.2
+    particle_patches = get_patches(Lx,Ly,a,b)
 
-	patch_color_dict = {0:'red', 2:'yellow'}
+    patch_color_dict = {0:'red', 2:'yellow'}
 
-	# make frame directory if it doesn't exist
-	if not os.path.isdir("./frames"):
-		os.mkdir("./frames")
+    # make frame directory if it doesn't exist
+    if not os.path.isdir("./frames"):
+        os.mkdir("./frames")
 
-	for j,val in enumerate(check_point_values[1:]):
-		pos_i = np.fromfile("positions_{}.bin".format(val))
-		pos_i = np.reshape(pos_i, (-1,3))
-		pos_i = pos_i[:,:2]
-		orient_i = np.fromfile("orientations_{}.bin".format(val))
-		orient_i = np.reshape(orient_i, (-1,5))[:,4]
+    for j,val in enumerate(check_point_values[1:]):
+        pos_i = np.fromfile("positions_{}.bin".format(val))
+        pos_i = np.reshape(pos_i, (-1,3))
+        pos_i = pos_i[:,:2]
+        orient_i = np.fromfile("orientations_{}.bin".format(val))
+        orient_i = np.reshape(orient_i, (-1,5))[:,4]
 
-		fb = open("patch_energy_{}.bin".format(val), "rb")
-		patch_i = np.fromfile(fb, dtype=np.int32)
-		patch_i = np.reshape(patch_i, (-1,4))[:,:2]
-		N=len(pos_i)
-		patch_i = patch_i[:N]
+        fb = open("patch_energy_{}.bin".format(val), "rb")
+        patch_i = np.fromfile(fb, dtype=np.int32)
+        patch_i = np.reshape(patch_i, (-1,4))[:,:2]
+        N=len(pos_i)
+        patch_i = patch_i[:N]
+        fig,ax = plt.subplots()
+        ax.set_aspect('equal', 'box')
+        print(N, network_arr[j].shape)
+        # domain_colors.shape = (N,) ( colors per particle )
+        domain_colors = get_domain_colors(N, network_arr[j], length_color_dict)
 
-		fig,ax = plt.subplots()
-		ax.set_aspect('equal', 'box')
+        for i in range(N):
+            rect = patches.Rectangle((-Lx/2,-Ly/2),
+            Lx,Ly, linewidth=0.5, edgecolor='k',facecolor=domain_colors[i], alpha=0.7)
+            theta = (orient_i[i]*180)/(np.pi)
+            r = mpl.transforms.Affine2D().rotate_deg_around(0,0,theta)
+            t = mpl.transforms.Affine2D().translate(pos_i[i,0],pos_i[i,1])
 
-		# domain_colors.shape = (N,) ( colors per particle )
-		domain_colors = get_domain_colors(N, network_arr[j], length_color_dict)
+            patch_1 = patches.Circle((particle_patches[0,0],particle_patches[0,1]),radius=radius,
+            facecolor=patch_color_dict[patch_i[i,0]])
+            patch_2 = patches.Circle((particle_patches[1,0],particle_patches[1,1]),radius=radius,
+            facecolor=patch_color_dict[patch_i[i,1]])
 
-		for i in range(N):
-			rect = patches.Rectangle((-Lx/2,-Ly/2),
-			Lx,Ly, linewidth=0.5, edgecolor='k',facecolor=domain_colors[i], alpha=0.7)
-			theta = (orient_i[i]*180)/(np.pi)
-			r = mpl.transforms.Affine2D().rotate_deg_around(0,0,theta)
-			t = mpl.transforms.Affine2D().translate(pos_i[i,0],pos_i[i,1])
+            tra = r + t + ax.transData
+            rect.set_transform(tra)
+            patch_1.set_transform(tra)
+            patch_2.set_transform(tra)
 
-			patch_1 = patches.Circle((particle_patches[0,0],particle_patches[0,1]),radius=radius,
-			facecolor=patch_color_dict[patch_i[i,0]])
-			patch_2 = patches.Circle((particle_patches[1,0],particle_patches[1,1]),radius=radius,
-			facecolor=patch_color_dict[patch_i[i,1]])
+            ax.add_patch(rect)
+            ax.add_patch(patch_1)
+            ax.add_patch(patch_2)
 
-			tra = r + t + ax.transData
-			rect.set_transform(tra)
-			patch_1.set_transform(tra)
-			patch_2.set_transform(tra)
-
-			ax.add_patch(rect)
-			ax.add_patch(patch_1)
-			ax.add_patch(patch_2)
-
-		ax.scatter(pos_i[:,0], pos_i[:,1],s=1)
-		ax.set_title("Frame {}".format(j))
-		plt.xlim((-1,80))
-		plt.ylim((-1,70))
-		plt.grid()
-		plt.savefig("./frames/frame_{}.png".format(j), dpi=300)
-		plt.close()
+        ax.scatter(pos_i[:,0], pos_i[:,1],s=1)
+        ax.set_title("Frame {}".format(j))
+        plt.xlim((-1,80))
+        plt.ylim((-1,70))
+        plt.grid()
+        plt.savefig("./frames/frame_{}.png".format(j), dpi=300)
+        plt.close()
