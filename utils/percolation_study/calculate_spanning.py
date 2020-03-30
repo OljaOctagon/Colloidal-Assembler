Added Ternary feature to source and para.ini / spanning tree with correct pbc/graph calculation
source/*:
Added ternary feature to source. Now three types of particles possible.
Note: Particle types hard coded in particles.cpp/Grand_Canonical.cpp

param/para.ini:
Added ternary on/off to enable ternary particle mix; added third chemical
potential - one chemical potential for each particle type.
Note: source expects input for each chemical potential. The additioanl
chemical potentials will be ignored in case binary or mono systems.

utils/percolation_study/calulate_spanning.py
Spanning tree calculation noww with correct pbc/graph calculation.
Note: for dma-as1,phi=0.5,T=0.1: fraction of spanning cluster is 1, while fraction of original largest cluster is 0.94 - Todo:  test if correct.

utils/percolation_study/calculate_spanning.py
@@ -6,21 +6,68 @@ import networkx as nx
 import glob
 import os
 
-# calculate if spanning:
-# use the virtual position
-# get pdist and get patch dist
-# calculate all connections again
-# get largest cluster
+def get_edge_points(pos_i,ax_n,sign_p):
+    edge_n = np.zeros(2)
+    edge_n = pos_i + sign_p[0]*ax_n[:,0]/2. + sign_p[1]*ax_n[:,1]/2.
+
+    return edge_n
+
+def get_orient(v, rot_mat):
+    return rot_mat.dot(v)
 
 
-# NOTE: probalbly deprecated
-def calculate_pbc_images(pos_i,box_l,patches_i, particles_max_domain):
+def rotation_matrix(theta):
+    rot_mat = np.zeros((2,2))
 
-    N_largest = len(particles_max_domain)
-    # all periodic images of largest cluster
+    rot_mat[0,0] = np.cos(theta) 
+    rot_mat[0,1] = -np.sin(theta)
+    rot_mat[1,0] = np.sin(theta)
+    rot_mat[1,1] = np.cos(theta)
+
+    return rot_mat
+
+
+def get_patches(pid,orient_pid,pos_pid):
+
+    patches_pid = np.zeros((4,2))
+    sin60 = np.sin(np.pi/3.)
+    cos60 = np.cos(np.pi/3.)
+
+    ax0 = np.array([[1,cos60],[0,sin60]])
+    edges = np.zeros((4,2))
+    ax_n = np.zeros((2,2))
+
+    rotmat_pid = rotation_matrix(orient_pid)
+    ax_n = get_orient(ax0, rotmat_pid)
+
+    edges[0] = get_edge_points(pos_pid,ax_n,np.array([-1,-1]))
+    edges[1] = get_edge_points(pos_pid,ax_n,np.array([+1,-1]))
+    edges[2] = get_edge_points(pos_pid,ax_n,np.array([+1,+1]))
+    edges[3] = get_edge_points(pos_pid,ax_n,np.array([-1,+1]))
+
+    # dma as1 type
+
+    # patch type 1 
+    patches_pid[0,:] = edges[0] + 0.2*(edges[3]-edges[0])
+    patches_pid[1,:] = edges[2] + 0.8*(edges[3]-edges[2])
+
+    # patch type 2 
+    patches_pid[2,:] = edges[0] + 0.2*(edges[1]-edges[0])
+    patches_pid[3,:] = edges[1] + 0.2*(edges[2]-edges[1])
+
+    return patches_pid
+
+
+
+def calculate_pbc_images(pos_i,orient_i,box_l):
+
+    # Periodic images of all particles 
     N_images = 9
-    N_virtual = N_largest*N_images
+    N_patches = 4 
+    N_virtual = N_particles*N_images
     virtual_pos = np.zeros((N_virtual,2))
+    virtual_orient = np.zeros((N_virtual))
+
     sign_array = np.array([[0,0],
                             [-1,0],
                             [-1,-1],
@@ -31,26 +78,13 @@ def calculate_pbc_images(pos_i,box_l,patches_i, particles_max_domain):
                             [0,1],
                             [-1,1]])
 
-    for k,pid in enumerate(particles_max_domain):
+    for pid in (range(N_particles)):
         for image_j in range(N_images):
-            virtual_pos[k+image_j*N_largest] = pos_i[pid] + sign_array[image_j]*box_l
+            new_id = pid+image_j*N_particles
+            virtual_pos[new_id] = pos_i[pid] + sign_array[image_j]*box_l
+            virtual_orient[new_id] = orient_i[pid]
 
-
-    return virtual_pos
-
-# NOTE: probalbly deprecated
-def get_pdist(vpos):
-    l = len(vpos)
-    pdist = np.zeros((l,l,2))
-    for i in range(2):
-        p = np.reshape(vpos[:,i], (l,1))
-        pdist = p - p.transpose()
-
-    N_pdist = np.sqrt(
-        np.power( pdist[:,:,0], 2)
-        + np.power( pdist[:,:,:1], 2))
-
-    return pdist, N_pdist
+    return virtual_pos, virtual_orient
 
 
 if __name__ == '__main__':
@@ -68,7 +102,8 @@ if __name__ == '__main__':
     frac_largest = []
     virtual_frac_largest = []
 
-    for j,val in enumerate(check_point_values[1:]):
+    for j,val in enumerate(check_point_values[-1:]):
+        print("checkpoint time ", val)
         pos_i = np.fromfile("positions_{}.bin".format(val))
         pos_i = np.reshape(pos_i, (-1,3))
         pos_i = pos_i[:,:2]
@@ -79,80 +114,63 @@ if __name__ == '__main__':
         box_l = box_all[3:5]
 
         # Make a graph for time j: 
-        connections_j = connections[j]
+        connections_j = connections[-1]
         G=nx.Graph()
         G.add_edges_from(connections_j[:,:2])
         particles_max_domain = gt.get_particles_in_largest_cluster(G)
         N_largest = len(particles_max_domain)
 
         frac_largest_i = N_largest/N_particles
+        print(frac_largest_i)
         frac_largest.append(frac_largest_i)
         virtual_frac_largest_i = 0 
         if frac_largest_i > 0.5:
-
-            patches_i = np.zeros((N,4,2))
             sin60 = np.sin(np.pi/3.)
             cos60 = np.cos(np.pi/3.)
+            N_images = 9 
+
+            print("Calculate virtual positions")
             cutoff = np.sqrt(np.power((1 + cos60),2) + np.power(sin60,2)) + 0.1
             patch_cutoff = 0.1
+            virtual_pos, virtual_orient = calculate_pbc_images(pos_i,orient_i,box_l)
+            virtual_box = box_l*3
+            N_patches = 4
+            N_virtual = N_particles*N_images
 
-            ax0 = np.array([[1,cos60],[0,sin60]])
-            edges = np.zeros((4,2))
-            ax_n = np.zeros((2,2))
+            virtual_patch_network = []
 
-            for pid in particles_max_domain:
-                rotmat_i = rotation_matrix(orient_i[pid])
-                ax_n = get_orient(ax0, rotmat_i)
-
-                edges[0] = get_edge_points(pos_i[pid],ax_n,np.array([-1,-1]))
-                edges[1] = get_edge_points(pos_i[pid],ax_n,np.array([+1,-1]))
-                edges[2] = get_edge_points(pos_i[pid],ax_n,np.array([+1,+1]))
-                edges[3] = get_edge_points(pos_i[pid],ax_n,np.array([-1,+1]))
-
-                # dma as1 type
-
-                # patch type 1 
-                patches_i[pid,0,:] = edges[0] + 0.2*(edges[3]-edges[0])
-                patches_i[pid,1,:] = edges[2] + 0.8*(edges[3]-edges[2])
-
-                # patch type 2 
-                patches_i[pid,2,:] = edges[0] + 0.2*(edges[1]-edges[0])
-                patches_i[pid,3,:] = edges[1] + 0.2*(edges[2]-edges[1])
-
-
-           for pid1 in particles_max_domain:
-               for pid2 in particles_max_domain:
+            print("Calculate next neighbours and bonds")
+            # Next get all particles that are next neighbours, and calc if patches meet
+            for pid in range(N_virtual):
+                patches_pid = get_patches(pid,virtual_orient[pid],virtual_pos[pid])
+                for nid in range(N_virtual):
+                    if pid<nid:
+                        pdist = virtual_pos[pid] - virtual_pos[nid]
+                        pdist = pdist - virtual_box*np.rint(pdist/virtual_box)
+                        Ndist = np.linalg.norm(pdist)
+                        if Ndist<cutoff: 
+                            patches_nid = get_patches(nid,virtual_orient[nid],virtual_pos[nid])
+                            for patch_pid in range(N_patches):
+                                for patch_nid in range(N_patches):
+                                    p = patch_pid - patch_nid
+                                    patch_dist = p - virtual_box*np.rint(p/virtual_box)
+                                    N_patch_dist = np.linalg.norm(patch_dist)
+                                    if N_patch_dist < patch_cutoff:
+                                        virtual_patch_network.append([pid,nid])
 
 
-
-
-
-
-
-
-
-
-''' 
-            # calculate virtual connections of image particles 
-            for connection in connections_j:
-                id1_0 = connection[0]
-                id2_0 = connection[1]
-                if id1_0 in particles_max_domain and id2_0 in particles_max_domain:
-                    for image_j in range(N_images):
-                        id1 = id1_0 + image_j*N_largest
-                        id2 = id2_0 + image_j*N_largest
-                        virtual_connections.append([id1,id2])
-
-            # make a new graph of virtual particles, calculate the largest
+            print("Calculate bonded neighbour graph")
+            # get virtual Graph
             G_virtual  = nx.Graph()
-            G_virtual.add_edges_from(virtual_connections)
+            G_virtual.add_edges_from(virtual_patch_network)
+
+            print("Calculate largest cluster")
             virtual_max_domain = gt.get_particles_in_largest_cluster(G_virtual)
             virtual_frac_largest_i = len(virtual_max_domain)/(N_particles*N_images)
 
-        virtual_frac_largest.append(virtual_frac_largest_i)
+            virtual_frac_largest.append(virtual_frac_largest_i)
 
     with open("spanning.dat", 'w') as f:
         f.write("time,fraction_largest, fraction_largest_virtual\n")
-        for j,val in enumerate(check_point_values[1:]):
+        for j,val in enumerate(check_point_values[-1:]):
             f.write("{},{},{}\n".format(val,frac_largest[j], virtual_frac_largest[j]))
-'''