d2eba847a4666647e111d276ceeddc6ee098d4fe
Cat Nate <catnat@protonmail.com>
2020-07-22 10:38:28 +0200

    draw rhombi with more ptypes; note: commit draw rhombi to master

--- utils/visual_inspection/draw_rhombi.py
+++ utils/visual_inspection/draw_rhombi.py
@@ -81,7 +81,7 @@ def get_domain_colors(N_particles, bond_arr, length_color_dict):
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Particle drawing methods')
-    parser.add_argument('-ptype', type=str, choices=['feq-as2', 'dma-as1', 'dmo-s1', 'dmo-s2', 'dmo-as1'])
+    parser.add_argument('-ptype', type=str, choices=['dma-as2', 'feq-as2', 'dma-as1', 'dmo-s1', 'dmo-s2', 'dmo-as1'])
     parser.add_argument('-delta', type=float)
     parser.add_argument('-radius', type=float)
 
