11365b62e054144f58c414502f7ec8273088db74
catnat <catnat@protonmail.com>
2020-07-30 11:35:36 +0200

    final test plot_bonds_per_particle.py - data was faulty, new input methods needed

--- utils/patch_size_variations/plot_bonds_per_particle.py
+++ utils/patch_size_variations/plot_bonds_per_particle.py
@@ -8,34 +8,33 @@ import networkx as nx
 import argparse
 import seaborn as sns
 import matplotlib.style as style
+import pandas as pd 
+
+
 style.use('seaborn-poster') 
 mpl.rcParams['font.family'] = "sans-serif"
 sns.set_context('poster')
 
 def read_bonds(filen):
-	first_line_pair = [0,0,0,0]
-	cut=False
-	with open(filen, 'r') as f:
-		network_list = []
-		for line in f:
-			if "#" in line:
-				network_list.append([])
-				first_line_pair = [0,0,0,0]
-				cut=False
+    
+    connection_soup = pd.read_csv(filen, delim_whitespace=True,header=None).values 
 
-			else:
-				line_counter=len(network_list[-1])
-				pairs = list(map(int, line.split(" ")))
-				if pairs == first_line_pair or cut==True:
-					cut=True
-				else:
-					network_list[-1].append(np.array(pairs))
+    delta_id = connection_soup[:-1,0] - connection_soup[1:,0]
+    cut_pos = np.where(delta_id>100)[0]  
+    
+    cut_pos = np.insert(cut_pos,0,-1)
+    cut_pos = np.insert(cut_pos,-1,len(connection_soup)-1)
+    network_list = []
+    
+    for i, pos_i in enumerate(cut_pos[:-1]):
 
-				if line_counter == 0:
-					first_line_pair = pairs
-	network_list = [ np.array(item) for item in network_list]
+        start=cut_pos[i]+1
+        end = cut_pos[i+1]
+        if( (end-start) > 100):
+            arr=connection_soup[start:end+1,:]    
+            network_list.append(arr)
 
-	return network_list
+    return network_list
 
 if __name__ == '__main__':
 
@@ -52,22 +51,28 @@ if __name__ == '__main__':
     # --------------------------
 
     # network_arr format: network_arr.shape = ( frame_i, bond_rows_frame_i )
-     network_arr = read_bonds("patch_network.dat")
+    network_arr = read_bonds("patch_network.dat")
     # patch position calculation
 
-    for j,val in enumerate(check_point_values[-10:]):
+    print("test:", len(network_arr), len(check_point_values))
 
-        network_arr[j]
-        uniques, counts = np.unique(network_arr[j], return_counts=True, axis=1)
+    for j, arr in enumerate(network_arr[-1:]):
+        
+        arr_id = arr[:,:2]
+        
+        print("arr", arr_id.shape)
+        arr_id_sorted = np.sort(arr_id)
 
+        uniques, counts = np.unique(arr_id_sorted, return_counts=True, axis=0)
+        
         av_counts = np.mean(counts)
         max_counts = np.max(counts)
 
-        n_double = np.sum(counts>2)
+        n_double = np.sum(counts>1)
         percent_n_double = n_double/len(counts)
-
-        print(av_counts, max_counts, n_double, percent_n_double)
-
-
-
+        
+        print("av counts", av_counts)
+        print("max_counts", max_counts)
+        print("n_double", n_double)
+        print("percent_n_double", percent_n_double)
 
