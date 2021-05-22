9ef07d14bb56df90c555ac5aded52e763d5a8175
catnat <catnat@protonmail.com>
2021-05-22 18:18:57 +0200

    fixed bug in dat_to_xyz.py time scrap in loop instead of outside

--- utils/percolation_study/dat_to_xyz.py
+++ utils/percolation_study/dat_to_xyz.py
@@ -1,24 +1,39 @@
 import numpy as np
 import pandas as pd 
-
-file='PolymerPos.00001.T00001.dat'
-df = pd.read_csv(file, 
-        delim_whitespace=True, names=['time','x','y','z'])
-
-dg = pd.read_csv("LinkerPos.00001.T00001.dat", 
-        delim_whitespace=True, names=['time','pid', 'lid','x','y','z','var1','var2'])
+import os 
+from glob import glob 
 nbonds=30
-arr = ((dg.pid-1)*nbonds + (dg.lid-1)).values
-monomer_type = np.array((['N']*len(df)))
-monomer_type[arr] = 'C'
 
-df['type'] = monomer_type 
+odir="/home/zoettl/Results/data/EllPoly/2019b/visual_inspection/std_conditions/00111/poly/pos-files"
 
-Ntimes = np.sort(df.time.unique())
-print(Ntimes)
-for time in Ntimes:
-    arr =  df[df.time==time][['type', 'x','y','z']].values
-    with open("biogel_{}.xyz".format(time),'w') as f:
-        for [mtype, x,y,z,] in arr:
-            f.write("{}   {}   {}   {}\n".format(mtype,x,y,z))
+poly_files = glob("{}/PolymerPos.00001.T*.dat".format(odir))
+
+save_dir = "/home/zoettl/Results/data/EllPoly/2019b/visual_inspection/std_conditions/00111/poly/xyz-files"
+os.mkdir(save_dir)
+
+for file in poly_files:
+    df = pd.read_csv(file, 
+            delim_whitespace=True, names=['time','x','y','z'])
+
+    linker_id = file.split(".")[2]
+    dg = pd.read_csv("{}/LinkerPos.00001.{}.dat".format(odir, linker_id), 
+            delim_whitespace=True, names=['time','pid', 'lid','x','y','z','var1','var2'])
+    
+
+    Ntimes = np.sort(df.time.unique())
+    for time in Ntimes:
+        if time%100 == 0:
+
+            dh = df[df.time == time]
+            ds = dg[dg.time == time]
+    
+            arr = ((ds.pid-1)*nbonds + (ds.lid-1)).values
+            monomer_type = np.array((['M']*len(dh)))
+            monomer_type[arr] = 'C'
+            dh['type'] = monomer_type 
+
+            brr = dh[['type', 'x','y','z']].values
+            with open("{}/biogel_{}.xyz".format(save_dir, time),'w') as f:
+                for [mtype, x,y,z,] in brr:
+                    f.write("{}   {}   {}   {}\n".format(mtype,x,y,z))
 
