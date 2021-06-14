2ddfe4a3b0fc9cb48675cbb7dff9a6832c510456
catnat <catnat@protonmail.com>
2021-06-14 09:13:37 +0200

    cluster size color bugfree 2

--- utils/biogel/pymol_inspect_clusters.py
+++ utils/biogel/pymol_inspect_clusters.py
@@ -9,15 +9,23 @@ directory=
 bfile = "{}/{}/{}/biogel_cluster_color_1000.0.xyz".format(directory, name,id)
 cmd.load(bfile,"e0")
 
-df = pd.read_csv(bfile, names=['mtype','x','y','z'], delim_whitespace=True)
-unique_val = df.mtype.unique()
+#df = pd.read_csv(bfile, names=['mtype','x','y','z'], delim_whitespace=True)
+#unique_val = df.mtype.unique()
 
 cmd.show_as("spheres")
-cmd.alter('elem C', "vdw=0.7")
-cmd.alter('elem C', "color=30")
-for i,mi in enumerate(unique(val)):
-    cmd.alter('elem'+str(mi), "color="+str(i)
-    cmd.alter('elem'+str(mi), "vdw=0.5")
+cmd.alter('all', 'vdw=0.3')
+cmd.alter('all', 'color=4')
+
+cmd.alter('elem C', "vdw=0.4")
+cmd.alter('elem C', "color=10")
+
+cmd.alter('elem M13', "vdw=0.4")
+cmd.alter('elem M13', "color=2")
+
+#print(unique_val)
+#for i,mi in enumerate(unique_val):
+#    cmd.alter('elem '+str(mi), "color="+str(i))
+#    cmd.alter('elem '+str(mi), "vdw=0.5")
 
 cmd.bg_color("white")
 cmd.set("orthoscopic")
