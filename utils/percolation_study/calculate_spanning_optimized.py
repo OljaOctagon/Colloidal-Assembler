04567561d6fca1807a89ab598a19d8b0f2e0bd88
Cat Nate <catnat@protonmail.com>
2021-08-31 10:29:25 +0200

    optimized spanning algorithm

--- utils/percolation_study/calculate_spanning_optimized.py
+++ utils/percolation_study/calculate_spanning_optimized.py
@@ -5,87 +5,19 @@ import gel_tools as gt
 import networkx as nx
 import glob
 import os
+from itertools import islice
+from collections import deque
 
-def get_edge_points(pos_i,ax_n,sign_p):
-    edge_n = np.zeros(2)
-    edge_n = pos_i + sign_p[0]*ax_n[:,0]/2. + sign_p[1]*ax_n[:,1]/2.
+def push(i,j,rot,cell_lists):
+    network_list=[]
+    for klist in cell_lists:
+        for k in klist:
+            im_j = j + k*N_particles
+            l=islice(deque(klist).rotate(rot),k,None)
+            im_i = i + l*N_particles
+            network_list.append([im_i,im_j])
 
-    return edge_n
-
-def get_orient(v, rot_mat):
-    return rot_mat.dot(v)
-
-
-def rotation_matrix(theta):
-    rot_mat = np.zeros((2,2))
-
-    rot_mat[0,0] = np.cos(theta) 
-    rot_mat[0,1] = -np.sin(theta)
-    rot_mat[1,0] = np.sin(theta)
-    rot_mat[1,1] = np.cos(theta)
-
-    return rot_mat
-
-
-def get_patches(pid,orient_pid,pos_pid):
-
-    patches_pid = np.zeros((4,2))
-    sin60 = np.sin(np.pi/3.)
-    cos60 = np.cos(np.pi/3.)
-
-    ax0 = np.array([[1,cos60],[0,sin60]])
-    edges = np.zeros((4,2))
-    ax_n = np.zeros((2,2))
-
-    rotmat_pid = rotation_matrix(orient_pid)
-    ax_n = get_orient(ax0, rotmat_pid)
-
-    edges[0] = get_edge_points(pos_pid,ax_n,np.array([-1,-1]))
-    edges[1] = get_edge_points(pos_pid,ax_n,np.array([+1,-1]))
-    edges[2] = get_edge_points(pos_pid,ax_n,np.array([+1,+1]))
-    edges[3] = get_edge_points(pos_pid,ax_n,np.array([-1,+1]))
-
-    # dma as1 type
-
-    # patch type 1 
-    patches_pid[0,:] = edges[0] + 0.2*(edges[3]-edges[0])
-    patches_pid[1,:] = edges[2] + 0.8*(edges[3]-edges[2])
-
-    # patch type 2 
-    patches_pid[2,:] = edges[0] + 0.2*(edges[1]-edges[0])
-    patches_pid[3,:] = edges[1] + 0.2*(edges[2]-edges[1])
-
-    return patches_pid
-
-
-
-def calculate_pbc_images(pos_i,orient_i,box_l):
-
-    # Periodic images of all particles 
-    N_images = 9
-    N_patches = 4 
-    N_virtual = N_particles*N_images
-    virtual_pos = np.zeros((N_virtual,2))
-    virtual_orient = np.zeros((N_virtual))
-
-    sign_array = np.array([[0,0],
-                            [-1,0],
-                            [-1,-1],
-                            [0,-1],
-                            [1,-1],
-                            [1,0],
-                            [1,1],
-                            [0,1],
-                            [-1,1]])
-
-    for pid in (range(N_particles)):
-        for image_j in range(N_images):
-            new_id = pid+image_j*N_particles
-            virtual_pos[new_id] = pos_i[pid] + sign_array[image_j]*box_l
-            virtual_orient[new_id] = orient_i[pid]
-
-    return virtual_pos, virtual_orient
-
+    return network_list 
 
 if __name__ == '__main__':
 
@@ -101,13 +33,19 @@ if __name__ == '__main__':
     frac_largest = []
     virtual_frac_largest = []
 
+
+    cell_dict = {'top-bottom': [[2,1,8][3,0,7],[4,5,6]],
+                    'left-right':[[2,3,4],[1,0,5],[8,7,6]],
+                    'diag-top-bottom':[[2,0,6],[3,5,8],[4,1,7]],
+                    'diag-bottom-top': [[8,0,4],[1,3,6],[2,7,5]]}
+
+
+
     for j,val in enumerate(check_point_values[-1:]):
         print("checkpoint time ", val)
         pos_i = np.fromfile("positions_{}.bin".format(val))
         pos_i = np.reshape(pos_i, (-1,3))
         pos_i = pos_i[:,:2]
-        orient_i = np.fromfile("orientations_{}.bin".format(val))
-        orient_i = np.reshape(orient_i, (-1,5))[:,4]
         N_particles=len(pos_i)
         box_all = np.fromfile("Box_{}.bin".format(val))
         box_l = box_all[3:5]
@@ -125,28 +63,78 @@ if __name__ == '__main__':
         frac_largest.append(frac_largest_i)
 
         virtual_frac_largest_i = 0 
-
-        # Calculate distances between next neighbours (bonded particles according to patch_network) and choose if pbc are hit or not
-        # if pbc not hit:
-        # particles remain neighbours and can be filled into new network edge array, and all periodic iamges are neighbours 
-        # if pbc are hit:
-        # particles are neighoburs to neighbours according to where the particles reaches out of the box
-
-
-        # no need to calculate virtual positions
-
-
-        virtual_box_l = box_l*3
         virtual_patch_network = []
+        N_images = 9
 
-        def calculate_distances(i,j):
-            pdist = pos[i] - pos[j]
-            pdist = pdist - box_l*np.rint(pdist/box_l)
-            # probably won't need that
-            #Ndist = np.linalg.norm(pdist)
-    
-            return pdist 
+        for conn_j in connections:
+            i,j = conn_j
 
+            # Calculate distances between next neighbours
+            # (bonded particles according to patch_network) 
+            pdist = pos[j]-pos[i]
+
+            x = pdist[0]
+            y=  pdist[1]
+
+            # define some shorthands 
+            x_abs = np.fabs(x)
+            x_sign = np.sign(x)
+            y_abs = np.fabs(y)
+            y_sign= np.sign(y)
+
+            box_x=box_l[0]/2
+            box_y=box_l[1]/2
+
+            # if pbc not hit:
+            # particles remain neighbours and are filled into new network edge array
+            if (x_abs < box_x) and (y_abs < box_y):
+                for k in N_images:
+                    virtual_patch_network.append(i+k*N,j+k*N)
+
+            # pbc are hit and particles are neighbours with image according to cell lists
+
+            else: 
+                # top-bottom
+                if (x_abs < box_x) and (y_abs > box_y):
+                    if y_sign > 0:
+                        rot = 1
+                    if y_sign < 0:
+                        rot = -1
+
+                    network_list_images = push(i,j,rot,cell_dict['top-bottom'])
+                    virtual_patch_network.extend(network_list_images)
+
+                # left-right
+                if (x_abs > box_x) and (y_abs < box_y):
+                    # to the left
+                    if x_sign > 0:
+                        rot=-1
+                    # to the right
+                    if x_sign < 0:
+                        rot=1
+
+                    network_list_images = push(i,j,rot,cell_dict['left-right'])
+                    virtual_patch_network.extend(network_list_images)
+
+                # diagognal  
+                if (x_abs > box_x) and (y_abs > box_y):
+                    # left top to right bottom 
+                    if x_sign != y_sign:
+                        if x_sign > 0 and y_sign < 0:
+                            rot = -1
+                        if x_sign < 0 and y_sign > 0:
+                            rot = 1
+                        network_list_images = push(i,j,rot,cell_dict['diag-top-bottom'])
+                        virtual_patch_network.extend(network_list_images)
+
+                    # left bottom to right top 
+                    if x_sign == y_sign:
+                        if x_sign > 0:
+                            rot = 1
+                        if x_sign < 0:
+                            rot = -1
+                        network_list_images = push(i,j,rot,cell_dict['diag-bottom-top'])
+                        virtual_patch_network.extend(network_list_images)
 
         print("Calculate bonded neighbour graph")
         # get virtual Graph
