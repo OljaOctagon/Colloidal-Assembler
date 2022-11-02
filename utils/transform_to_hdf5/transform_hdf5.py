7bd35b349bff5a8cbdc6f37873835a34d167a9ce
Cat Nate <catnat@protonmail.com>
2022-11-02 14:00:06 +0100

    hdf5 transform raw version done

--- utils/transform_to_hdf5/transform_hdf5.py
+++ utils/transform_to_hdf5/transform_hdf5.py
@@ -1,12 +1,10 @@
-from asyncore import dispatcher_with_send
-from tkinter import E, W
 import numpy as np
 import h5py
 import glob
-import configparser 
+import configparser
 
-# create hd5f file 
-f = h5py.File("mytestfile.hdf5", 'a') 
+# create hd5f file
+f = h5py.File("mytestfile.hdf5", 'a')
 
 # read all bin binary files of directory
 subdirs = glob.glob("double*")
@@ -21,38 +19,39 @@ for dir in subdirs:
     grp.attrs['packing_fraction'] = configparser['System']['Packing_Fraction']
     grp.attrs['temperature'] = configparser['System']['Temperature']
     grp.attrs['number_cells'] = configparser['System']['Number_of_Cells']
-    
+
     grp.attrs['calculation_frequency'] = configparser['Output']['Calculation_Frequency']
     grp.attrs['checkpoint_frequency'] = configparser['Output']['Checkpoint_Frequency']
     grp.attrs['sigma_translation'] = configparser['Monte_Carlo_Parameters']['Sigma_Translation']
     grp.attrs['sigma_rotation'] = configparser['Monte_Carlo_Parameters']['Sigma_Rotation']
 
     grp.attrs['ptype'] = configparser['Rhombus']['rhombus_type']
-    grp.attrs['energy'] = configparser['Rhombus']['Energy_Level'] 
+    grp.attrs['energy'] = configparser['Rhombus']['Energy_Level']
     grp.attrs['patch_delta'] = configparser['Rhombus']['patch_delta']
     grp.attrs['patch_size'] = configparser['Rhombus']['patch_size']
 
-    grp.attrs['ensemble']='NVT'
+    grp.attrs['ensemble'] = 'NVT'
 
     box = np.fromfile("Box_{}.bin".format(val))
     grp.attrs['box_xcenter'] = box[0]
     grp.attrs['box_ycenter'] = box[1]
-    grp.attrs['box_lx'] =  box[3]
+    grp.attrs['box_lx'] = box[3]
     grp.attrs['box_ly'] = box[4]
 
     checkpoints = glob.glob("{}/positions.*bin".format(dir))
     check_point_values = np.sort(
-    [int(point.split("_")[-1].split(".")[0]) for point in checkpoints]
+        [int(point.split("_")[-1].split(".")[0]) for point in checkpoints]
     )
 
-    N=int(grp.atrrs['number_particles'])
+    N = int(grp.atrrs['number_particles'])
     for j, val in enumerate(check_point_values):
 
-        x_extent = N 
-        pos_dim=2
-        orient_dim = 1 
-        y_extent = pos_dim + orient_dim 
-        dset = f.create_dataset("checkpoint_{}".format(j), (x_extent,y_extent), dtype='f')
+        x_extent = N
+        pos_dim = 2
+        orient_dim = 1
+        y_extent = pos_dim + orient_dim
+        dset = grp.create_dataset("checkpoint_{}".format(
+            j), (x_extent, y_extent), dtype='f')
 
         patch_i = np.fromfile("patch_energy_{}.bi".format(val))
         dset.attrs['patch_type_0'] = patch_i[0]
@@ -69,7 +68,8 @@ for dir in subdirs:
         orient_i = np.fromfile("orientations_{}.bin".format(val))
         orient_i = np.reshape(orient_i, (-1, 5))[:, 4]
 
-        orient_i = orient_i.reshape(-1,1)
-        config_i = np.concatenate((pos_i,orient_i),axis=1)
+        orient_i = orient_i.reshape(-1, 1)
+        config_i = np.concatenate((pos_i, orient_i), axis=1)
         dset[...] = config_i
 
+f.close()
