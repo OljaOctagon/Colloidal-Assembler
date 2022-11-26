4522df3fcca31d04e43fa16465f339d3f358d684
Cat Nate <catnat@protonmail.com>
2022-11-26 15:30:01 +0100

    changed energy plotting

--- utils/percolation_study/plot_energy.py
+++ utils/percolation_study/plot_energy.py
@@ -30,35 +30,33 @@ Delta = [0.2, 0.3, 0.4]
 Temps = ['0.01', '0.02', '0.03', '0.04, 0.05', '0.06', '0.07', '0.08', '0.09', '0.10',
          '0.11', '0.12', '0.13', '0.14', '0.15', '0.16']
 
-
-len_delta = len(Delta)
+len_phi = len(Phi)
 len_temps = len(Temps)
 
-fig, ax = plt.subplots(len_temps, len_delta, figsize=(10, 20))
+fig, ax = plt.subplots(len_temps, len_phi, figsize=(10, 20))
 plt.rcParams["axes.prop_cycle"] = plt.cycler(
     "color", plt.cm.viridis(np.linspace(0, 1, 23)))
 
-for i, ti in enumerate(Temps):
-    for j, delta in enumerate(Delta):
-        axi = ax[i, j]
-        axi.set_yscale("log")
+for delta in Delta:
+    for i, ti in enumerate(Temps):
+        for j, phi in enumerate(Phi):
+            axi = ax[i, j]
+            axi.set_yscale("log")
+            for ri in range(1, 9):
+                file_i = '{}_phi_{}_delta_{}_temp_{}_run_{}/Energy.dat'.format(
+                    pdict[args.ptype], phi, delta, ti, ri)
 
-        for phi in Phi:
-            file_i = '{}_phi_{}_delta_{}_temp_{}/Energy.dat'.format(
-                pdict[args.ptype], phi, delta, ti)
+                arr = pd.read_csv(file_i, delim_whitespace=True).values
+                arr[:, 1] = -1*arr[:, 1]
+                axi.plot(arr[:, 0], arr[:, 1], lw=0.3,
+                         label="run id = {}".format(ri))
 
-            #temp = float(re.findall(r"[-+]?\d*\.\d+|\d+",file_i)[3])
-            arr = pd.read_csv(file_i, delim_whitespace=True).values
-
-            arr[:, 1] = -1*arr[:, 1]
-            axi.plot(arr[:, 0], arr[:, 1], lw=0.3,
-                     label="$\\phi = ${}".format(phi))
-
-        axi.set_title("$\Delta$={},T={}".format(delta, ti))
-        axi.set_xlabel("sweeps")
-        axi.set_ylabel("abs(U)")
-        box = axi.get_position()
-        axi.set_position([box.x0, box.y0, box.width * 0.65, box.height*0.8])
-plt.tight_layout()
-plt.legend(loc=1, bbox_to_anchor=(0.2, -0.3), ncol=6)
-plt.savefig("{}_energy.pdf".format(pdict[args.ptype]))
+            axi.set_title("$\phi$={},T={}".format(phi, ti))
+            axi.set_xlabel("sweeps")
+            axi.set_ylabel("abs(U)")
+            box = axi.get_position()
+            axi.set_position(
+                [box.x0, box.y0, box.width * 0.65, box.height*0.8])
+    plt.tight_layout()
+    plt.legend(loc=1, bbox_to_anchor=(0.2, -0.3), ncol=6)
+    plt.savefig("{}_{}_energy.pdf".format(delta, pdict[args.ptype]))
