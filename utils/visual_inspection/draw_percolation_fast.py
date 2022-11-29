35828a0bc11706bf5a3ea9e7bbda3f952a7ebdd4
Cat Nate <catnat@protonmail.com>
2022-11-29 22:06:39 +0100

    added get all frames to fast drawing

--- utils/visual_inspection/draw_percolation_fast.py
+++ utils/visual_inspection/draw_percolation_fast.py
@@ -9,8 +9,9 @@ import configparser
 import re
 
 
-def generator_from_fsys(fsys_iterator):
+def generator_from_fsys():
 
+    fsys_iterator = glob.glob("double*/double*/")
     for dir_i in fsys_iterator:
         config = configparser.ConfigParser()
         config.read('{}para.ini'.format(dir_i))
@@ -46,12 +47,34 @@ def generator_from_fsys(fsys_iterator):
         yield (fid, ptype, phi, temperature, delta, last_time, pos, orient, box)
 
 
+def generator_from_single_dir():
+
+    pos_iterator = glob.glob('positions_*.bin')
+    # get the last value from the string
+    def g(x): return int(re.findall(r'\d+', x)[-1])
+
+    for posf_i in pos_iterator:
+        stime = g(posf_i)
+        pos_file = "positions_{}.bin".format(stime)
+        pos = np.fromfile(pos_file)
+        pos = np.reshape(pos, (-1, 3))
+        pos = pos[:, :2]
+        orient_file = "{}orientations_{}.bin".format(stime)
+        orient = np.fromfile(orient_file)
+        orient = np.reshape(orient, (-1, 5))[:, 4]
+
+        box_file = "{}Box_{}.bin".format(stime)
+        box = np.fromfile(box_file)
+
+        yield (stime, pos, orient, box)
+
+
 def draw(particles, box, frame_name):
     scale = 100
     img = np.full(
         (ceil(box.lx * scale), ceil(box.ly * scale), 3), 255, np.uint8)
     for vert_i in particles.vertices:
-        cv.fillPoly(img, np.int32([vert_i * scale]), (153, 153, 255))
+        cv.fillPoly(img, np.int32([vert_i * scale]), (255, 153, 153))
 
     outsize = (10000, 10000)
     out = cv.resize(img, outsize)
@@ -78,15 +101,42 @@ def get_frames(vals):
     draw(particles, box, frame_name)
 
 
+def get_all_frames(vals):
+    stime, pos, orient, box = vals
+    blx = box[3]
+    ndim = 2
+    side_length = 1
+
+    # Generate particles object
+    Np = len(pos)
+    particles = pt.Rhombi(pos, orient, side_length, ndim)
+
+    # Generate box object
+    origin = box[:2] - blx/2.
+    box = pt.Box(origin, blx, ndim)
+
+    frame_name = "rhombi_{}.png".format(stime)
+
+    draw(particles, box, frame_name)
+
+
 if __name__ == '__main__':
 
     # read data either through files system via glob or via db
     parser = argparse.ArgumentParser()
     parser.add_argument('-ncores', type=int)
+    parser.add_argument('-it', type=str, choices=('all_dirs', 'all_pos'))
 
     args = parser.parse_args()
 
-    gen_fsys = generator_from_fsys(glob.glob("double*/double*/"))
+    fdict = {'all_dirs': generator_from_fsys,
+             'all_pos': generator_from_single_dir}
+
+    gdict = {'all_dirs': get_frames,
+             'all_pos': get_all_frames}
+
+    gen_fsys = fdict[args.it]()
+    gframes = gdict[args.it]
 
     N_CORES = int(args.ncores)
     N_CORES_MAX = 8
@@ -94,14 +144,15 @@ if __name__ == '__main__':
     if N_CORES > 1 and N_CORES <= N_CORES_MAX:
         print("Multiprocessing with {} cores".format(N_CORES))
         pool = multiprocessing.Pool(N_CORES)
-        pool.map(get_frames, gen_fsys)
+        #pool.map(get_frames, gen_fsys)
+        pool.map(gframes, gen_fsys)
         pool.close()
         pool.join()
 
     if N_CORES == 1:
         print("single core job")
         for vals in gen_fsys:
-            results = get_frames(vals)
+            results = gframes(vals)
 
     if N_CORES > N_CORES_MAX:
         print("Too many cores allocated, please do not use more than {} cores".format(
